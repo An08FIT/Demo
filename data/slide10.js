e3190c01a22e5f00f15d4f4f8fe0857a6fec1cfc
AndyKai66 <juantienan08@gmail.com>
2023-04-24 01:00:07 +0700

    first commit

---
+++ data/slide10.js
@@ -0,0 +1,16 @@
+(function(){var loadHandler=window['sl_{FAD37855-828F-4788-B29C-9F28B8083710}'];loadHandler&&loadHandler(9, '<div id="spr0_30e72a20"><div id="spr1_30e72a20" class="kern slide"><img id="img7_30e72a20" src="data/img6.png" width="960px" height="540px" alt="" style="left:0px;top:0px;"/><div id="spr3_30e72a20" style="top:210.211px;"><div id="spr4_30e72a20"><img id="img0_30e72a20" src="data/img0.png" width="318" height="330" alt="" style="left:-0.063px;top:-0.106px;"/></div></div><div id="spr5_30e72a20" style="top:227.744px;"><div id="spr6_30e72a20"><img id="img1_30e72a20" src="data/img1.png" width="120" height="186" alt="" style="left:-0.063px;top:0.128px;"/></div></div><div id="spr7_30e72a20" style="left:677.875px;top:132px;"><img id="img2_30e72a20" src="data/img2.png" width="222" height="222" alt=""/></div><div id="spr8_30e72a20" style="left:629.875px;"><div id="spr9_30e72a20"><img id="img3_30e72a20" src="data/img3.png" width="126" height="90" alt="" style="left:0.125px;top:-0.063px;"/></div></div><div id="spr10_30e72a20" style="left:677.628px;top:480px;"><div id="spr11_30e72a20"><img id="img4_30e72a20" src="data/img4.png" width="78" height="60" alt="" style="left:0.123px;"/></div></div><div id="spr12_30e72a20" style="left:820px;top:-1px;"><img id="img5_30e72a20" src="data/img5.png" width="60" height="96" alt=""/></div></div><div id="spr2_30e72a20" class="kern slide"><div id="spr13_30e72a20" style="left:50.875px;top:35.647px;"><div style="width:0px;"><span id="txt0_30e72a20" data-width="303.331055" style="left:7.2px;top:5.837px;">Phép toán tập hợp</span></div></div><div id="spr14_30e72a20" style="left:96px;top:170px;"><img id="img6_30e72a20" src="data/img9.png" width="676" height="268" alt="Quan hệ là tập hợp các bộ\
+Phép hợp 𝑅∪𝑆\
+Phép giao 𝑅∩𝑆\
+Phép trừ 𝑅−𝑆\
+Tính khả hợp (tương tích đồng nhất – Union Compatibility): Hai lược đồ quan hệ 𝑅( 𝐴 1 , 𝐴 2 ,…, 𝐴 𝑛 ) và S( 𝐵 1 , 𝐵 2 ,…, 𝐵 𝑛 ) là khả hợp (tương thích) nếu:\
+Cùng bậc n\
+Có 𝐷𝑂𝑀  𝐴 𝑖  =𝐷𝑂𝑀  𝐵 𝑖  ,1≤𝑛≤𝑛\
+Kết quả của ∪,∩,− là quan hệ có cùng tên thuộc tính với quan hệ đầu tiên (R) \
+\
+\
+\
+\
+\
+\
+\
+"/></div></div></div>', '{"s":[]}');})();
---
+++ data/slide7.js
@@ -0,0 +1,15 @@
+(function(){var loadHandler=window['sl_{FAD37855-828F-4788-B29C-9F28B8083710}'];loadHandler&&loadHandler(6, '<div id="spr0_30e7258c"><div id="spr1_30e7258c" class="kern slide"><img id="img7_30e7258c" src="data/img6.png" width="960px" height="540px" alt="" style="left:0px;top:0px;"/><div id="spr3_30e7258c" style="top:210.211px;"><div id="spr4_30e7258c"><img id="img0_30e7258c" src="data/img0.png" width="318" height="330" alt="" style="left:-0.063px;top:-0.106px;"/></div></div><div id="spr5_30e7258c" style="top:227.744px;"><div id="spr6_30e7258c"><img id="img1_30e7258c" src="data/img1.png" width="120" height="186" alt="" style="left:-0.063px;top:0.128px;"/></div></div><div id="spr7_30e7258c" style="left:677.875px;top:132px;"><img id="img2_30e7258c" src="data/img2.png" width="222" height="222" alt=""/></div><div id="spr8_30e7258c" style="left:629.875px;"><div id="spr9_30e7258c"><img id="img3_30e7258c" src="data/img3.png" width="126" height="90" alt="" style="left:0.125px;top:-0.063px;"/></div></div><div id="spr10_30e7258c" style="left:677.628px;top:480px;"><div id="spr11_30e7258c"><img id="img4_30e7258c" src="data/img4.png" width="78" height="60" alt="" style="left:0.123px;"/></div></div><div id="spr12_30e7258c" style="left:820px;top:-1px;"><img id="img5_30e7258c" src="data/img5.png" width="60" height="96" alt=""/></div></div><div id="spr2_30e7258c" class="kern slide"><div id="spr13_30e7258c" style="left:50.875px;top:35.647px;"><div style="width:0px;"><span id="txt0_30e7258c" data-width="250.769531" style="left:7.2px;top:5.837px;">Đại số quan hệ</span></div></div><div id="spr14_30e7258c" style="left:96px;top:163px;"><img id="img6_30e7258c" src="data/img8.png" width="286" height="311" alt="Biến là các quan hệ\
+Tập hợp (set)\
+Toán tử là các phép toán (operations)\
+Đổi tên 𝜌\
+Trên tập hợp\
+	Hội ∪ (union)\
+	Giao ∩ (intersec)\
+	Trừ - (difference)\
+Rút chích 1 phần của quan hệ\
+	Chọn 𝜎 (selection)\
+	Chiếu 𝜋 (projection)\
+Kết hợp các quan hệ\
+	Tích Cartesian × (Cartesian product)\
+	Kết ⋈ (join)\
+"/></div></div></div>', '{"s":[]}');})();
